e1fc5ac769c4d175c48370111044e654472a7b5f
JedBelhaj <jedbelhaj05@gmail.com>
2025-05-24 14:25:24 +0100

    starting rest

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
 const express = require("express");
-const http = require("http");
 const cors = require("cors");
-const { Server } = require("socket.io");
-const { handleSocketEvents } = require("./utils/socketUtils");
+
+const PORT = 5000;
 
 const app = express();
 app.use(cors());
 
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
-});
-
-app.get("/", (req, res) => {
-  res.status(200).send("Hi!");
-});
-
-io.on("connection", (socket) => {
-  console.log(`User connected: ${socket.id}`);
-  handleSocketEvents(io, socket);
-});
-const port = process.env.PORT | 5000;
-server.listen(port, () => {
-  console.log("Server listening on port 3000...");
+app.listen(PORT, () => {
+  console.log(`Api Server Running on port ${PORT}`);
 });
